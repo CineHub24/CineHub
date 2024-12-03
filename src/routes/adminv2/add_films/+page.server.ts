938144dacf4db231a1c5c86f455fac4650bf5593
Jochern <johannes.kling@outlook.de>
2024-12-03 23:46:30 +0100

    added some types

--- src/routes/adminv2/add_films/+page.server.ts
+++ src/routes/adminv2/add_films/+page.server.ts
@@ -1,18 +1,35 @@
 import { film } from '$lib/server/db/schema';
 import { year } from 'drizzle-orm/mysql-core';
-import type { Actions } from './$types';
-import { error } from '@sveltejs/kit';
 import { db } from '$lib/server/db';
+import { error, type Actions, type RequestEvent } from '@sveltejs/kit';
 
+import * as table from '$lib/server/db/schema';
 
-let filmSearch:{id:string, title:string, type:string, year: number, poster:string}[];
-let completeFilmInformation:{title:string, type:string, genre: string, director:string,runtime:string, ageRating:string, poster:string, description:string,releaseDate:string }[];
+type Movie = {
+    id: number;
+    title: string;
+    year: number;
+    poster: string; 
+};
 
+type CompleteMovieInformation = { 
+    Title:string, 
+    Type:string, 
+    Year: number, 
+    Poster:string,
+    Runtime:string, 
+    Genre:string, 
+    Director:string, 
+    Plot:string, 
+    Release:Date 
+};
 
+let movies:Movie[];
+let completeMovieInformation:CompleteMovieInformation[];
 
 export const actions = {
     
-    search: async ({request}) => {
+    search: async ({request}:RequestEvent) => {
         const formdata =  await request.formData();
         const query = formdata.get("query");
 
@@ -23,7 +40,7 @@ export const actions = {
             const res = await fetch(`http://www.omdbapi.com/?apikey=b97fe887&s=${query}`);
             const data = await res.json();
             console.log(data)
-            filmSearch= data.Search.map((movie: { imdbID: string; Title: any; Type:any; Year:any; Poster:string }) =>({
+            movies  = data.Search.map((movie: any) =>({
                 id: movie.imdbID,
                 title: movie.Title,
                 type: movie.Type,
@@ -33,8 +50,8 @@ export const actions = {
             // return{
             //     movies: films
             // }
-        } catch(e) {
-            throw error(500, "API")
+        } catch(e:unknown){ 
+            throw error(500, "searchError: " + e)   
         }
         
         
@@ -48,11 +65,13 @@ export const actions = {
 
        try {
         const res = await fetch(`http://www.omdbapi.com/?apikey=b97fe887&plot=full&i=${id}`);
-        const data = await res.json();
-        console.log(data)
-        completeFilmInformation = data.map((movie:{ Title:string, Type:string, Year: number, Poster:string,Runtime:string, Genre:string, Director:string, Plot:string, Release:Date } ) => ({
 
-            
+        if (!res.ok) {
+            throw error(res.status, 'Failed to fetch data from external API');
+        }
+
+        const data = await res.json();
+        completeMovieInformation = data.map((movie:CompleteMovieInformation) => ({
                 title: movie.Title,
                 type: movie.Type,
                 year: movie.Year,
@@ -64,16 +83,11 @@ export const actions = {
                 releaseDate: movie.Release 
 
         }));
-        // console.log(completeFilmInformation)
+
         await db.insert(film).values(data)
-
-
        }catch(e){
         console.log("error" + e)
        }
- 
-
-        
     }
     
 } satisfies Actions;
@@ -82,9 +96,9 @@ export const actions = {
 
 
 export const load = async (event) => {
-   
+
     return{
-        movies: filmSearch
+        movies: movies
     }
 
   };
