96dbd507293448ad73fddd63100f2e6454facb90
Mika Jun <mika.jun@sap.com>
2024-12-13 10:07:23 +0100

    Minor Fix

--- src/routes/admin/priceSet/+page.svelte
+++ src/routes/admin/priceSet/+page.svelte
@@ -1,18 +1,21 @@
 <script>
-    export let data;
-    const {priceSets} = data
+	export let data;
+
+	const { priceSets } = data;
 </script>
 
-<div class="container mx-auto px-4">
-<h1 class="text-3xl font-bold mb-4">Alle Preissets</h1>
-<ul class="divide-y divide-gray-200">
-    {#each priceSets as priceSet}
-    <li class="py-4 flex justify-between">
-        <span>{priceSet.name}</span>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Bearbeiten
-        </button>
-    </li>
-    {/each}
+<h1>Alle Preissets</h1>
+
+<ul>
+	{#each priceSets as priceSet}
+		<li>
+			{priceSet.name}
+
+			<button
+				on:click={() => {
+					/* Hier navigierst du zur Bearbeitungsseite */
+				}}>Bearbeiten</button
+			>
+		</li>
+	{/each}
 </ul>
-</div>
