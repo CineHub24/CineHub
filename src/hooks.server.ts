1bde18f074e3fac1755aae3e4ece9650934277f5
Felix Erhard <felix.erhard@sap.com>
2024-12-16 19:19:29 +0100

    Added Routeprotection für /admin

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,9 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { i18n } from '$lib/i18n';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
+import { goto } from '$app/navigation';
+import Header from '$lib/components/header.svelte';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
@@ -24,5 +26,23 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	return resolve(event);
 };
 
+const handleAdmin: Handle = async ({ event, resolve }) => {
+	const user = event.locals.user;
+	if (user && user.role === 'admin') {
+		return resolve(event);
+	} else {
+		return redirect(302, '/');
+		
+	}
+};
+
 const handleParaglide: Handle = i18n.handle();
-export const handle: Handle = sequence(handleAuth, handleParaglide);
+
+const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/admin')) {
+		return sequence(handleAuth, handleAdmin, handleParaglide)({ event, resolve });
+	}
+	return sequence(handleAuth, handleParaglide)({ event, resolve });
+};
+
+export { handle };
