f26912221b56c1f4bfc32ee045271cf64517b268
Luca Bäck <luca.baeck@sap.com>
2024-12-17 10:41:55 +0100

    add films show image

--- src/routes/admin/add_films/+page.server.ts
+++ src/routes/admin/add_films/+page.server.ts
@@ -6,131 +6,128 @@ import { languageAwareRedirect } from '$lib/utils/languageAware';
 
 export type Movie = typeof film.$inferSelect;
 
-
 export type CompleteMovieInformation = {
-    imdbID: string;
-    title: string;
-    year: number;
-    poster: string;
-    runtime: number;
-    genre: string[];
-    ageRating: string,
-    director: string;
-    description: string;
+	imdbID: string;
+	title: string;
+	year: number;
+	poster: string;
+	runtime: number;
+	genre: string[];
+	ageRating: string;
+	director: string;
+	description: string;
 };
 
 type filmForInsert = typeof film.$inferInsert;
 
-
 export const actions = {
-    
-    search: async ({request}:RequestEvent) => {
-        const formdata =  await request.formData();
-        const query = formdata.get("query");
+	search: async ({ request }: RequestEvent) => {
+		const formdata = await request.formData();
+		const query = formdata.get('query');
 
-        if (!query) throw error(400,"Query is required");
+		if (!query) throw error(400, 'Query is required');
 
-        
-        try {
-            const res = await fetch(`http://www.omdbapi.com/?apikey=b97fe887&s=${query}`);
-            const data = await res.json();
-            console.log(data)
-            const movies: Movie[] = data.Search.map((movie: any) => ({
-                imdbID: movie.imdbID,
-                title: movie.Title,
-                type: movie.Type,
-                releaseDate: movie.ReleaseDate,
-                poster: movie.Poster
-            }));
+		try {
+			const res = await fetch(`http://www.omdbapi.com/?apikey=b97fe887&s=${query}`);
+			const data = await res.json();
+			if (data.Error) return {};
+			const movies: Movie[] = data.Search.map((movie: any) => ({
+				imdbID: movie.imdbID,
+				title: movie.Title,
+				type: movie.Type,
+				year: movie.Year,
+				poster: movie.Poster
+			}));
 
-            return {
-                movies: movies
-            }
+			return {
+				movies: movies
+			};
+		} catch (e: unknown) {
+			throw error(500, 'searchError: ' + e);
+		}
+	},
 
-        } catch(e:unknown){ 
-            throw error(500, "searchError: " + e)   
-        }
-    },
+	fetchFullMovieDetails: async ({ request }) => {
+		const data = await request.formData();
+		const movieId = data.get('id');
 
-    fetchFullMovieDetails: async ({ request }) => {
-        const data = await request.formData();
-        const movieId = data.get('id');
+		try {
+			// Fetch full movie details from OMDB API
+			const response = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=b97fe887&`);
+			const fullMovieDetails = await response.json();
 
-        try {
-            // Fetch full movie details from OMDB API
-            const response = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=b97fe887&`);
-            const fullMovieDetails = await response.json();
+			const movieResponse: CompleteMovieInformation = {
+				imdbID: fullMovieDetails.imdbID,
+				title: fullMovieDetails.Title,
+				poster: fullMovieDetails.Poster,
+				genre: fullMovieDetails.Genre,
+				director: fullMovieDetails.Director,
+				runtime: fullMovieDetails.Runtime,
+				ageRating: fullMovieDetails.Rated,
+				description: fullMovieDetails.Plot,
+				year: fullMovieDetails.Year
+			};
 
-            const movieResponse: CompleteMovieInformation = {
-                imdbID: fullMovieDetails.imdbID,
-                title: fullMovieDetails.Title,
-                poster: fullMovieDetails.Poster,
-                genre: fullMovieDetails.Genre,
-                director: fullMovieDetails.Director,
-                runtime: fullMovieDetails.Runtime,
-                ageRating: fullMovieDetails.Rated,
-                description: fullMovieDetails.Plot,
-                year: fullMovieDetails.Year
-            }
+			//genres -> extre tabellen?
+			return {
+				type: 'success',
+				data: {
+					movie: JSON.stringify(movieResponse)
+				}
+			};
+		} catch (error) {
+			console.error('Error fetching movie details:', error);
+			return {
+				type: 'failure',
+				error: 'Could not fetch movie details'
+			};
+		}
+	},
+	save: async ({ request }) => {
+		const formData = await request.formData();
 
-            //genres -> extre tabellen?
-            return {
-                type: 'success',
-                data: {
-                    movie: JSON.stringify(movieResponse),
-                }
-            };
-        } catch (error) {
-            console.error('Error fetching movie details:', error);
-            return {
-                type: 'failure',
-                error: 'Could not fetch movie details'
-            };
-        }
-    },
-    save: async ({ request }) => {
-        const formData = await request.formData();
+		function extractNumberFromRuntime(runtime: string | null | undefined): number | null {
+			if (!runtime) return null;
 
-        function extractNumberFromRuntime(runtime: string | null | undefined): number | null {
-            if (!runtime) return null;
+			// Use a regex to extract the first number from the string
+			const match = runtime.match(/\d+/);
+			return match ? Number(match[0]) : null;
+		}
 
-            // Use a regex to extract the first number from the string
-            const match = runtime.match(/\d+/);
-            return match ? Number(match[0]) : null;
-        }
+		const movieToSave: filmForInsert = {
+			imdbID: formData.get('imdbID')?.toString(),
+			title: formData.get('title')?.toString(),
+			genres: formData
+				.get('genres')
+				?.toString()
+				.split(',')
+				.map((e) => e.trim()),
+			director: formData.get('director')?.toString(),
+			runtime: extractNumberFromRuntime(formData.get('runtime')?.toString()),
+			ageRating: formData.get('actors')?.toString(),
+			poster: formData.get('poster')?.toString(),
+			description: formData.get('plot')?.toString(),
+			year: formData.get('year')?.toString()
+		};
 
-        const movieToSave: filmForInsert = {
-            imdbID: formData.get("imdbID")?.toString(),
-            title: formData.get("title")?.toString(),
-            genres: formData.get("genres")?.toString().split(',').map(e => e.trim()), 
-            director: formData.get("director")?.toString(),
-            runtime: extractNumberFromRuntime(formData.get("runtime")?.toString()),
-            ageRating: formData.get("actors")?.toString(),
-            poster: formData.get("poster")?.toString(),
-            description: formData.get("plot")?.toString(),
-            year: formData.get("year")?.toString()
-        };
+		let success = false;
+		let filmId: number;
 
-        let success = false;
-        let filmId: number;
+		try {
+			if (!movieToSave.imdbID || !movieToSave.title) {
+				// Maybe add more
+				throw error(400, 'Missing required movie information');
+			}
 
-        try {
-            if (!movieToSave.imdbID || !movieToSave.title) {    // Maybe add more
-                throw error(400, 'Missing required movie information');
-            }
+			[{ filmId }] = await db.insert(film).values(movieToSave).returning({ filmId: film.id });
 
-            [{ filmId }] = await db.insert(film).values(movieToSave).returning({ filmId: film.id });;
+			console.log(filmId);
 
-            console.log(filmId)
-
-            success = true;
-
-        } catch (e) {
-            console.error("Error saving movie:", e);
-            throw error(500, 'Failed to save movie');
-        }
-        if (success) throw languageAwareRedirect(302, `/admin/films/film/${filmId}`);
-    }
+			success = true;
+		} catch (e) {
+			console.error('Error saving movie:', e);
+			throw error(500, 'Failed to save movie');
+		}
+		if (success) throw languageAwareRedirect(302, `/admin/films/film/${filmId}`);
+	}
 } satisfies Actions;
-
-
