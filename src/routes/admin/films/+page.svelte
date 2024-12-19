6389caac9e45d58fce01c526c84cebf909fc587f
Luca Bäck <luca.baeck@sap.com>
2024-12-19 16:48:42 +0100

    small ui fixes

--- src/routes/admin/films/+page.svelte
+++ src/routes/admin/films/+page.svelte
@@ -9,15 +9,16 @@
 
 </script>
 <div class="bg-white p-6 rounded-lg shadow">
-    <h1 class="text-2xl font-bold mb-4">Filmverwaltung</h1>
-    <div class="flex justify-end mb-4">
-        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onclick={() => {
-            languageAwareGoto('/admin/film/create');
-        }}>
+    <div class="flex justify-between items-center mb-8">
+        <h1 class="text-2xl font-bold">Filmverwaltung</h1>
+        <button 
+            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" 
+            onclick={() => languageAwareGoto('/admin/film/create')}
+        >
             Neuer Film
         </button>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div class="grid grid-cols-[repeat(auto-fill,minmax(175px,1fr))] gap-5">
         {#each movies as movie}
             <MovieCard {movie} url="/admin/film/{movie.id}" />
         {/each}
