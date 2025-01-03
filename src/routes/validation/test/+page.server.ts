2185340112d5b5f6ac509e50703424e93f937e51
Felix Erhard <felix.erhard@sap.com>
2025-01-03 19:04:55 +0100

    emailService: Möglichkeit Tickets + cancelMails zu verschicken

--- src/routes/validation/test/+page.server.ts
+++ src/routes/validation/test/+page.server.ts
@@ -1,10 +1,30 @@
-import { generateQRCodeBase64 } from "$lib/utils/generateQR";
+import { EmailService } from '$lib/utils/emailService';
+import { generateQRCodeBase64 } from '$lib/utils/generateQR';
 
-export const load = async ({  }) => {
+export const load = async ({}) => {
+	const qrCode = await generateQRCodeBase64('1234');
+	const gmailUser = import.meta.env.VITE_GMAIL_USER;
+	const gmailAppPassword = import.meta.env.VITE_GMAIL_APP_PASSWORD;
+	const emailClient = new EmailService(gmailUser, gmailAppPassword);
+    console.log(gmailAppPassword, gmailUser)
 
-    const qrCode = await generateQRCodeBase64("1234");
+	const ticket = {
+		movieTitle: 'Inception',
+		date: '2021-12-24',
+		time: '20:00',
+		hall: 'Saal 1',
+		seat: 'A1',
+		id: '1234'
+	};
+	console.log('sending email');
+	// try {
+	// 	await emailClient.sendTicketConfirmation(ticket, 'felixb.erhard@gmail.com');
+	// 	console.log('email sent');
+	// } catch (error) {
+	// 	console.log(error);
+	// }
 
-    return {
-        code: qrCode
-    }
-}
+	return {
+		code: qrCode
+	};
+};
