a15279a7267bf4636a5c1277c65632277da7d270
Nico Wrede <nico.wrede@sap.com>
2025-01-06 15:25:23 +0100

    remove unused Stripe code

--- src/routes/cart/stripe/+page.svelte
+++ src/routes/cart/stripe/+page.svelte
@@ -1,19 +1,136 @@
-<script lang="ts">
+<script>
+// @ts-nocheck
+    import { goto } from '$app/navigation'
+    import { page } from '$app/stores'
     import { onMount } from 'svelte'
-    import { loadStripe, type Stripe } from '@stripe/stripe-js'
-    import type { PageServerData } from '../[id]/stripe/$types';
-	import { EmbeddedCheckout } from 'svelte-stripe';
+    import { loadStripe } from '@stripe/stripe-js'
+	  import { Elements, ExpressCheckout } from 'svelte-stripe';
     const PUBLIC_STRIPE_KEY = import.meta.env.VITE_PUBLIC_STRIPE_KEY
-
-    const data: PageServerData = $props();
-
-    let stripe: Stripe | null = $state(null);
-
+  
+    /**
+	 * @type {import("@stripe/stripe-js").Stripe | null}
+	 */
+    let stripe = null
+    /**
+	 * @type {{ message: any; } | null}
+	 */
+    let error = null
+    /**
+	 * @type {{ submit: () => any; }}
+	 */
+    let elements
+    let processing = false
+    // let stripe: Stripe | null = $state(null); // Stripe instance or null if not loaded
+    // let error: { message: string } | null = null; // Error object from Stripe or null
+    // let elements: ; // Elements instance from Stripe or undefined
+    // let processing: boolean = false; // Flag to indicate ongoing processing
+  
+    const bookingId = page.url.pathname.replace('/cart/', '').replace('/stripe', '');
     onMount(async () => {
-        stripe = await loadStripe(PUBLIC_STRIPE_KEY)
+      stripe = await loadStripe(PUBLIC_STRIPE_KEY)
     })
-</script>
-
-<h1>Embedded Checkout Example</h1>
-
-<EmbeddedCheckout {stripe} clientSecret={data.clientSecret}/>
+  
+    async function createPaymentIntent() {
+      const response = await fetch(page.url.pathname + '/payment-intent', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({})
+      })
+      const { clientSecret } = await response.json()
+  
+      return clientSecret
+    }
+  
+    /**
+	 * @param {{ detail: { resolve: (arg0: { emailRequired: boolean; phoneNumberRequired: boolean; lineItems: { name: string; amount: number; }[]; }) => void; }; }} event
+	 */
+    async function click(event) {
+      const options = {
+        emailRequired: true,
+        phoneNumberRequired: true,
+        lineItems: [
+          {
+            name: 'Tickets',
+            amount: 1099
+          }
+        ]
+      }
+  
+      event.detail.resolve(options)
+    }
+  
+    async function confirm() {
+      // avoid processing duplicates
+      if (processing) return
+  
+      processing = true
+  
+      let result = await elements.submit()
+  
+      if (result.error) {
+        // validation failed, notify user
+        error = result.error
+        processing = false
+        return
+      }
+  
+      // create payment intent server side
+      const clientSecret = await createPaymentIntent()
+      const return_url = new URL('/booking/' + bookingId, window.location.origin).toString()
+  
+      // confirm payment with stripe
+      // @ts-ignore
+      result = await stripe.confirmPayment({
+        // @ts-ignore
+        elements,
+        clientSecret,
+        confirmParams: {
+          return_url
+        }
+      })
+  
+      // log results, for debugging
+      console.log({ result })
+  
+      if (result.error) {
+        // payment failed, notify user
+        error = result.error
+        processing = false
+      } else {
+        // payment succeeded, redirect to "thank you" page
+        goto('/booking/' + bookingId)
+      }
+    }
+  </script>
+  
+  <h1>Express Checkout Example</h1>
+  
+  {#if error}
+    <p class="error">{error.message} Please try again.</p>
+  {/if}
+  
+  <Elements
+    {stripe}
+    mode="payment"
+    currency="eur"
+    amount={1099}
+    bind:elements>
+  
+    <ExpressCheckout
+      on:confirm={confirm}
+      on:click={click}
+      buttonHeight={50}
+      buttonTheme={{googlePay: 'white'}}
+      buttonType={{googlePay: 'donate'}}
+      paymentMethodOrder={['googlePay', 'link']}/>
+  
+  </Elements>
+  
+  <style>
+    .error {
+      color: tomato;
+      margin: 2rem 0 0;
+    }
+  </style>
--- src/routes/cart/stripev2/+page.server.ts
+++
@@ -1,3 +0,0 @@
-// fetch matching Tickets (ticket.bookingId = bookingId) from DB => tickets can be shown on their on in the checkout windows
-
-// add method that calculates the total price based on the singular tickets and the applied discount (read from booking table)
